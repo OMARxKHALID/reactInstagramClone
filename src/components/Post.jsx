8dacee9cf8100cf91f9911aff65deae84723a8aa
OMAR K <mirxaumar1212@gmail.com>
2024-02-16 14:33:14 +0500

    14th  commit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -44,7 +44,7 @@ const Post = ({ post }) => {
 
     return (
         <div className="py-1 px-3">
-            <div className="bg-gray-50 rounded-md mb-2 border-b-2 border-gray-300">
+            <div className="brounded-md mb-2 border-b-2 border-gray-300">
                 <div className="flex items-center sm:px-0 md:px-4 lg:px-4 xl:px-4 py-2 ml-1">
                     <img
                         src={user && user.url}
